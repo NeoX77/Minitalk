739111e1b8ad5d28d8a72516afb9754e0c6e4791
William DEBOTTE <wdebotte@e2r9p4.clusters.42paris.fr>
2022-02-02 16:11:48 +0100

    save

--- srcs/client.c
+++ srcs/client.c
@@ -6,20 +6,39 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 13:50:35 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/01/31 16:45:53 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/02/02 16:10:41 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
+static void	ft_send_char_to_server(char c_char, int i_serverpid)
+{
+	char	*s_binary;
+
+	s_binary = ft_itoa_base(c_char, "01");
+	while ()
+	{
+		:wq
+
+	}
+}
+
 int	main(int args, char **argv)
 {
-	(void)argv;
+	char	*s_str;
+
 	if (args != 3)
 	{
 		ft_putstr("Usage: ./client \"pid\" \"message\"\n");
 		exit(EXIT_FAILURE);
 	}
-
+	s_str = argv[2];
+	while (*s_str != '\0')
+	{
+		ft_send_char_to_server(*s_str, ft_atoi(argv[1]));
+		s_str++;
+//		pause();
+	}
 	exit(EXIT_SUCCESS);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,32 +6,29 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 13:50:23 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/01/30 17:59:32 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/02/02 16:10:36 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void	toto(int i_signum)
+static char	g_sbinary[7];
+
+void	get_binary(int i_signum)
 {
+	static	
 	if (i_signum == SIGUSR1)
-		printf("I received SIGUSR1 : %i\n", i_signum);
-	else if (i_signum == SIGUSR2)
-		printf("I received SIGUSR2 : %i\n", i_signum);
-	else
-		printf("coucou\n");
 }
 
 int	main(void)
 {
 	ft_putstr("The server pid is : ");
 	ft_putnbr(getpid());
-	ft_putchar('\n');
+	ft_putchar('\n');:wq
 	while (1)
 	{
-		signal(SIGUSR2, toto);
-		signal(SIGUSR1, toto);
-		signal(SIGQUIT, toto);
+		signal(SIGUSR1, get_binary);
+		signal(SIGUSR2, get_binary);
 		pause();
 	}
 }
